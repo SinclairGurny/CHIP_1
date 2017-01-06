a83a936675683132e2885b2970ea31542f949dd0
SinclairGurny <sinclairg@hotmail.com>
2017-01-05 23:48:40 -0500

    updated date_test.c

--- date_test.c
+++ date_test.c
@@ -1,10 +1,11 @@
+//Prints current time
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main() {
-	time_t now;
-	time(&now);
-	printf("Current Timer: %s\n", ctime(&now));
-	return 0;
+  time_t now;
+  time(&now);
+  printf("Current Timer: %s\n", ctime(&now));
+  return 0;
 }
