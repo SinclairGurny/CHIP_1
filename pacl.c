50bae5c9f44dfe935af73fd3870ad4b7b54d33a5
SinclairGurny <sinclairg@hotmail.com>
2017-01-08 01:05:28 -0500

    added some stuff

--- pacl.c
+++ pacl.c
@@ -4,28 +4,55 @@
 #include <unistd.h>
 
 #define ALARM_FILE "alarms.sg" //filetype?
-#define LAMP "1013"   //XI0
-#define SNOOZE "1015" //XI2
-#define SILENT "1017" //XI4
+#define LAMP "1014"   //XI0
+#define SNOOZE "1016" //XI2
+#define SILENT "1018" //XI4
+
+//TODO move to alarm.h
+struct alarm_t {
+  size_t hour;
+  size_t minute;
+  size_t light_pattern;
+  size_t week[7];
+}
+typedef struct alarm_t alarm;
+
+alarm _parse_to_alarm(char* str) {
+  return NULL;
+}
+
+void ring_alarm(alarm a) {
+  //check type of ring pattern
+}
+//==========================
 
 int main(int argc, char** argv) {
   //Read command line args
+    //-s set alarms
+    //-v view alarms
   
   //Set up GPIO pins
+  //add checks -- fail if not sudo
+  open_pin(LAMP); write_dir(LAMP, "out");
+  open_pin(SNOOZE); write_dir(SNOOZE, "in");
+  open_pin(SILENT); write_dir(SILENT, "in");
   
   //Read in alarms
+  FILE * alarm_file_ptr = fopen(ALARM_FILE, "r");
+  //do stuff
+    //make alarm structs from all alarms
   
   //RUN PROGRAM
-  
-  //Look for soonest alarm
   //If silent switch is off do not run alarms
   
-  //Sleep until alarm
-  
-  //ALARM
-    //Wait for snooze to be pressed to turn off
-  
-  //Close pins
+    //Look for soonest alarm
+
+    //Sleep until alarm
+
+    //ALARM
+      //Wait for snooze to be pressed to turn off
+
+    //Close pins
   
   return 0;
 }
