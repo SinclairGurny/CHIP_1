c2b15fa23b7f14a7545c6b2c41d77bc0c2fc56a8
SinclairGurny <sinclairg@hotmail.com>
2017-01-11 19:06:25 -0500

    new test

--- tests/auto.c
+++ tests/auto.c
@@ -3,10 +3,10 @@
 
 int main() {
   FILE * f = fopen("/home/chip/Desktop/foo.txt", "w");
-  if (f == NULL) { printf("Unsuccessful 1\n"); }
+  if (f == NULL) { printf("Unsuccessful 1\n"); return 1; }
   time_t now; struct tm* current;
   time(&now); current = localtime(&now);
-  fprintf(f, "Hello World :: %d:%d:%d\n", current->tm_hour, current->tm_min, current->tm_sec);
+  fprintf(f, "Hello World :: %02d:%02d:%02d\n", current->tm_hour, current->tm_min, current->tm_sec);
   fclose(f);
   printf("Successful 1\n");
   return 0;
